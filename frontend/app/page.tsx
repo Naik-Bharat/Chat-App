e8d1347b678fe0a8aeb722055b5cd8f4ca57e2b3
Bharat Naik <bharatnaik777@gmail.com>
2023-04-17 20:03:12 +0530

    added websocket logic on frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
@@ -5,8 +7,13 @@ import styles from './page.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const socket = new WebSocket("ws://localhost:8080/ws/123");
+  socket.onmessage = ({ data }) => {
+    console.log(data);
+  };
+
   return (
-    <main>
-    </main>
+    <div className='mx-6'>
+    </div>
   )
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/Naik-Bharat/chat-app/api"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/websocket/v2"
 )
 
@@ -18,6 +19,12 @@ func main() {
 	})
 
 	app.Get("/ws/:roomId", websocket.New(api.HandleWebSocket))
+
+	app.Use(cors.New())
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+	}))
 
 	app.Listen(":8080")
 }
