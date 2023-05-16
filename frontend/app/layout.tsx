3bfe7727c603df179b8e1d56586b47f5bbf5cb02
Bharat Naik <bharatnaik777@gmail.com>
2023-05-16 19:20:22 +0530

    changing to contatiner view

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -13,14 +13,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className='container mx-auto px-6'>
         <header className='py-7'>
           {/* <nav className='hidden sm:block'> */}
           <nav>
-            <div className='items-center mx-6 flex justify-between'>
+            <div className='items-center flex justify-between'>
               <div className='logo text-xl'>Chat App</div>
               <ul className='justify-between'>
-                <li className='mr-6'>
+                <li className=''>
                     <Link href='https://github.com/Naik-Bharat/chat-app' target={"_blank"}>
                       <button className='rounded-md bg-blue-800 px-3 py-1'>Star project on Github</button>
                     </Link>
