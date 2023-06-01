87ec6b56845de8306b437adba24b873d4ff97f7a
Bharat Naik <bharatnaik777@gmail.com>
2023-06-01 20:00:13 +0530

    moved header to components

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import Link from 'next/link'
+import Header from '@/components/Header'
 
 export const metadata = {
   title: 'Chat App',
@@ -14,16 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className='container mx-auto px-6 bg-white dark:bg-black text-black dark:text-white'>
-        <header className='py-7'>
-          <nav>
-            <div className='items-center flex justify-between'>
-              <div className='logo text-2xl font-bold'>Chat App</div>
-                <Link href='https://github.com/Naik-Bharat/chat-app' target={"_blank"}>
-                  <button className='rounded-md bg-blue-800 px-3 py-1 text-white'>Star project on Github</button>
-                </Link>
-            </div>
-          </nav>
-        </header>
+        <Header />
         {children}
       </body>
     </html>
