f6d5962519fa3716c1cd50df18b570dadb490bdd
Bharat Naik <bharatnaik777@gmail.com>
2023-06-06 23:41:57 +0530

    cleared input field once message has been sent

--- frontend/components/MessageInput.tsx
+++ frontend/components/MessageInput.tsx
@@ -15,11 +15,12 @@ const MessageInput = ({ handleMessageSubmission }: Props) => {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     handleMessageSubmission(data);
+    setData("");
   }
 
   return (
     <form onSubmit={handleSubmit} className='flex justify-evenly h-10 my-8'>
-      <input type='text' onChange={handleDataChange} placeholder='Type your message here...' autoFocus className='rounded-sm bg-zinc-100 dark:bg-zinc-900 px-2 py-2 flex-grow' />
+      <input type='text' onChange={handleDataChange} value={data} placeholder='Type your message here...' autoFocus className='rounded-sm bg-zinc-100 dark:bg-zinc-900 px-2 py-2 flex-grow' />
       <button type='submit' className='ml-3 rounded-full py-2 px-3 bg-blue-800'>
         <Image src='/send-btn.svg' alt='Send Button' width={16} height={16} />
       </button>
