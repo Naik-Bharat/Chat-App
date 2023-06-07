81ab14ab487601300fab2d6d4a6932c27b1ab27a
Bharat Naik <bharatnaik777@gmail.com>
2023-06-08 01:03:43 +0530

    adjusting height of textarea when sending message

--- frontend/components/MessageInput.tsx
+++ frontend/components/MessageInput.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 type Props = {
   handleMessageSubmission: (data: string) => void
@@ -8,14 +8,22 @@ type Props = {
 const MessageInput = ({ handleMessageSubmission }: Props) => {
   const [data, setData] = useState("");
   const formRef = useRef<HTMLFormElement>(null);
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    adjustTextAreaHeight();
+  }, [data]);
 
   const handleDataChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    // adjust height of textarea
-    event.target.style.height = "auto";
-    event.target.style.height = `${event.target.scrollHeight}px`;
-
     // change value of data
     setData(event.target.value);
+  }
+
+  const adjustTextAreaHeight = () => {
+    if (textAreaRef.current) {
+      textAreaRef.current.style.height = 'auto';
+      textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
+    }
   }
 
   const handleKeyPress = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
@@ -34,7 +42,7 @@ const MessageInput = ({ handleMessageSubmission }: Props) => {
 
   return (
     <form onSubmit={handleSubmit} ref={formRef} className='flex justify-evenly my-8'>
-      <textarea onChange={handleDataChange} onKeyDown={handleKeyPress} value={data} rows={1} placeholder='Type your message here...' autoFocus className='resize-none max-h-48 rounded-sm bg-zinc-100 dark:bg-zinc-900 px-2 py-2 h-auto flex-grow' />
+      <textarea onChange={handleDataChange} onKeyDown={handleKeyPress} ref={textAreaRef} value={data} rows={1} required placeholder='Type your message here...' autoFocus className='resize-none max-h-48 rounded-sm bg-zinc-100 dark:bg-zinc-900 px-2 py-2 h-auto flex-grow' />
       <button type='submit' className='ml-3 rounded-full self-end h-10 py-2 px-3 bg-blue-800'>
         <Image src='/send-btn.svg' alt='Send Button' width={16} height={16} />
       </button>
