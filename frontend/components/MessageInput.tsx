d423c563d22ddcaf88a0f301cfd5d9230f906366
Bharat Naik <bharatnaik777@gmail.com>
2023-06-08 00:31:56 +0530

    changed input field to textarea to include multi-line message

--- frontend/components/MessageInput.tsx
+++ frontend/components/MessageInput.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 type Props = {
   handleMessageSubmission: (data: string) => void
@@ -7,9 +7,23 @@ type Props = {
 
 const MessageInput = ({ handleMessageSubmission }: Props) => {
   const [data, setData] = useState("");
+  const formRef = useRef<HTMLFormElement>(null);
 
-  const handleDataChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleDataChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    // adjust height of textarea
+    event.target.style.height = "auto";
+    event.target.style.height = `${event.target.scrollHeight}px`;
+
+    // change value of data
     setData(event.target.value);
+  }
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    // handle submit when pressed enter
+    if (event.key == "Enter" && !event.shiftKey) {
+      event.preventDefault();
+      formRef.current?.dispatchEvent(new Event('submit', {bubbles: true, cancelable: true}));
+    }
   }
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -19,9 +33,9 @@ const MessageInput = ({ handleMessageSubmission }: Props) => {
   }
 
   return (
-    <form onSubmit={handleSubmit} className='flex justify-evenly h-10 my-8'>
-      <input type='text' onChange={handleDataChange} value={data} placeholder='Type your message here...' autoFocus className='rounded-sm bg-zinc-100 dark:bg-zinc-900 px-2 py-2 flex-grow' />
-      <button type='submit' className='ml-3 rounded-full py-2 px-3 bg-blue-800'>
+    <form onSubmit={handleSubmit} ref={formRef} className='flex justify-evenly my-8'>
+      <textarea onChange={handleDataChange} onKeyDown={handleKeyPress} value={data} rows={1} placeholder='Type your message here...' autoFocus className='resize-none max-h-48 rounded-sm bg-zinc-100 dark:bg-zinc-900 px-2 py-2 h-auto flex-grow' />
+      <button type='submit' className='ml-3 rounded-full self-end h-10 py-2 px-3 bg-blue-800'>
         <Image src='/send-btn.svg' alt='Send Button' width={16} height={16} />
       </button>
     </form>
